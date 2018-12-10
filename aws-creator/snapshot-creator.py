5ca1812efabe9562e258e9eaad930d1ab374d3c4
Anand Todkar <anand.todkar@siemens.com>
2018-12-09 19:34:05 -0500

    committed changes for ec2 update

--- aws-creator/snapshot-creator.py
+++ aws-creator/snapshot-creator.py
@@ -1,9 +1,25 @@
 import boto3
+import click
+
+session = boto3.Session()
+session.resource('ec2')
+
+def list_instances():
+    ec2s = session.resource('ec2')
+    for ec2 in ec2s.instances.all():
+        if ec2.tags is not None:
+            na = [name for name in ec2.tags if name['Key'] == "Name" and "ssi" in name['Value']]
+            if len(na) > 0:
+                print("EC2: " + str(na[0]['Value']) + " " + str(ec2.state))
+
+                if ec2.state['Name'] == 'stopped':
+                    ec2.start()
+                    print("Starting... ")
+                else:
+                    ec2.stop()
+                    print("Stopping... ")
+
 
 if __name__ == "__main__":
-    session = boto3.Session()
-    session.resource('ec2')
-    ec2s = session.resource('ec2')
-
-    for ec2 in ec2s.instances.all():
-        print(ec2)
+    #print(sys.argv)
+    list_instances()
